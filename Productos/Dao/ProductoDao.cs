d44a33cfbf9cc297c5c04621c7cd00be991f0e4c
Erickson Andrés López <ericksonlopez@uamv.edu.ni>
2025-09-22 10:52:27 -0600

    Quede en el formulario, se termino la clase Producto y ProductoDao.

---
+++ Productos/Dao/ProductoDao.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Productos.Dao
+{
+    public class ProductoDao
+    {
+        private ProductoDao[] carrito = new ProductoDao[10];
+        private int pos = 0;
+
+        public void Agregar(ProductoDao prod)
+        {
+            carrito[pos++] = prod;
+        }
+
+
+
+
+    }
+}
